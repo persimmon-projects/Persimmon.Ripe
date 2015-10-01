12a6fbf12f499762777635f45737c4202e3efb24
pocketberserker <pocketberserker@gmail.com>
2015-10-02 00:16:28 +0900

    init commit

---
+++ src/Persimmon.Ripe/AssemblyInfo.fs
@@ -0,0 +1,15 @@
+﻿namespace System
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitleAttribute("Persimmon.Ripe")>]
+[<assembly: AssemblyProductAttribute("Persimmon.Ripe")>]
+[<assembly: GuidAttribute("fb706671-5fe0-47e2-b1b5-879d651fb653")>]
+[<assembly: AssemblyDescriptionAttribute("")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1.0"
