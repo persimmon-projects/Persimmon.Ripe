d0a074a0e41473ab83bc22a40d39d2f8fd7a597d
pocketberserker <pocketberserker@gmail.com>
2015-10-24 20:08:57 +0900

    print executed test name

--- src/Persimmon.Ripe.Server/Program.fs
+++ src/Persimmon.Ripe.Server/Program.fs
@@ -1,11 +1,12 @@
-﻿open System.IO
+﻿open System
+open System.IO
 open Persimmon.Ripe
 open FsYaml
 
 [<EntryPoint>]
 let main argv =
   let config = Yaml.load<Config.RabbitMQ> (File.ReadAllText(argv.[0]))
-  use executor = new Executor(config)
+  use executor = new Executor(config, Console.Out)
   executor.Connect()
   while true do ()
   0
--- src/Persimmon.Ripe/Executor.fs
+++ src/Persimmon.Ripe/Executor.fs
@@ -8,21 +8,41 @@ open Config
 open Persimmon
 open Persimmon.ActivePatterns
 
-type Executor(config: RabbitMQ) =
+type Executor(config: RabbitMQ, writer: IO.TextWriter) =
 
   let connection = Connection.create config
   let channel = Connection.createChannel connection
   let serializer = FsPickler.CreateBinarySerializer()
   let publisher = new Publisher(config)
 
+  let rec writeResult prefix = function
+  | ContextResult ctx ->
+    ctx.Children
+    |> Seq.iter (writeResult (sprintf "%s.%s." prefix ctx.Name))
+  | EndMarker -> writer.WriteLine()
+  | TestResult tr -> fprintfn writer "end test: %s%s" prefix tr.FullName
+
   let run = function
-  | Context ctx -> ctx.Run(ignore) :> ITestResult
-  | TestCase tc -> tc.Run() :> ITestResult
+  | Context ctx -> ctx.Run(writeResult (sprintf "%s." ctx.Name)) :> ITestResult
+  | TestCase tc ->
+    let result = tc.Run() :> ITestResult
+    writeResult "" result
+    result
 
   let resultKey (key: string) =
     match key.Split([|'.'|]) with
     | [|_; key|] -> sprintf "%s.%s" RabbitMQ.Queue.Result key
     | _ -> RabbitMQ.Queue.Result
+
+  let receive (args: BasicDeliverEventArgs) =
+    let result =
+      try
+        let o = serializer.UnPickle<TestObject>(args.Body)
+        Success(run o)
+      with e -> Failure(args.Body, e)
+    result
+    |> Publisher.publish publisher RabbitMQ.Queue.Result (resultKey args.RoutingKey)
+    channel.BasicAck(args.DeliveryTag, false)
 
   member __.Connect() =
     channel.BasicQos(0u, 1us, false)
@@ -30,20 +50,14 @@ type Executor(config: RabbitMQ) =
     let queueName = channel.QueueDeclare(RabbitMQ.Queue.TestCase, false, false, false, null).QueueName
     channel.QueueBind(queueName, RabbitMQ.Exchange, sprintf "%s.*" RabbitMQ.Queue.TestCase)
     let consumer = EventingBasicConsumer(channel)
-    consumer.Received.Add(fun args ->
-      try
-        let o = serializer.UnPickle<TestObject>(args.Body)
-        Success(run o)
-      with e -> Failure(args.Body, e)
-      |> Publisher.publish publisher RabbitMQ.Queue.Result (resultKey args.RoutingKey)
-      channel.BasicAck(args.DeliveryTag, false)
-    )
+    consumer.Received.Add(receive)
     channel.BasicConsume(queueName, false, consumer) |> ignore
 
   member __.Dispose() =
     publisher.Dispose()
     channel.Dispose()
     connection.Dispose()
+    writer.Dispose()
 
   interface IDisposable with
     member this.Dispose() = this.Dispose()
