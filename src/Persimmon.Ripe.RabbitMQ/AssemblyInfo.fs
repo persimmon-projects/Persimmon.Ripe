32c49a6fa32e40cedd6f0b9bd904fe108b537211
pocketberserker <pocketberserker@gmail.com>
2015-10-26 01:49:27 +0900

    generate AssemblyInfo

---
+++ src/Persimmon.Ripe.RabbitMQ/AssemblyInfo.fs
@@ -0,0 +1,15 @@
+﻿namespace System
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitleAttribute("Persimmon.Ripe.RabbitMQ")>]
+[<assembly: AssemblyDescriptionAttribute("")>]
+[<assembly: GuidAttribute("7fb817b9-d3a0-4772-a04a-1aff163926b9")>]
+[<assembly: AssemblyProductAttribute("Persimmon.Ripe")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1.0"
